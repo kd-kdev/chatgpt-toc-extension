fb2e8a1e461ea47dd2a1448f34410b92efe7bd87
kd <kd.gamedeveloper@gmail.com>
2025-08-08 18:39:11 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+/**
+ * Defining constants
+ */
+const CONSTANTS = {
+    USER_MESSAGE: 'div[data-message-author-role="user"]'
+}
+
+/**
+ * Extracts user messages from ChatGPT frontend
+ */
+class UserMessageExtractor {
+
+}
+
+/**
+ * Main class
+ */
+class TOCExtension {
+
+}
+
+
+// test
+console.log("Content script loaded from ChatGPT ToC extension!");
+const div = document.createElement(div);
+div.textContent = "If you see this content script is loaded successfully!";
